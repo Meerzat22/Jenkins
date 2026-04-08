8fdb236dca75159c47813320419c0c550a3c3c9c
taalaibekovameerzat <taalajbekovameerzat@gmail.com>
2026-04-08 18:29:01 +0600

    test suits

--- src/test/java/demoQa/elements/TextBoxTest.java
+++ src/test/java/demoQa/elements/TextBoxTest.java
@@ -1,6 +1,7 @@
 package demoQa.elements;
 
 import demoQa.BaseTest;
+import io.qameta.allure.AllureId;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 @Tag("UI")
@@ -8,7 +9,7 @@ import org.junit.jupiter.api.Test;
 public class TextBoxTest extends BaseTest {
 
     @Test
-    @Tag("SMOKE")
+    @Tag("TextBoxTest")
     void textBoxTest(){
         driver.get("https://demoqa.com/text-box");
         textBoxPage.fillUserName("Naaame").
